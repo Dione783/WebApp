772047cad53fc5f19b8c0dc9216e27b1f62a8632
Brend <Brend@DESKTOP-GQOJ798>
2022-07-11 15:41:49 -0300

    Adicionar arquivos de projeto.

---
+++ WebApp/Controllers/SuperHeroController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private List<SuperHero> heroes = new List<SuperHero> 
+        { 
+        new SuperHero { Name = "Spider Man", firstName = "Pyter", lastName = "Parker", Id = 0,locality="New York"},
+        new SuperHero {Name="Ironman",firstName="Tony",lastName="Stark",locality="Long Island",Id=1},
+        new SuperHero {Name="Super Man",firstName="Caléo",lastName="",locality="New York",Id=2}
+        };
+        private readonly DataContext context;
+
+        public SuperHeroController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(await context.superHeroes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+        {
+            var hero = await context.superHeroes.FindAsync(id);
+            if(hero == null)
+            {
+                return BadRequest("Hero not finded");
+            }
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddHero(SuperHero hero)
+        {
+            context.superHeroes.Add(hero);
+            await context.SaveChangesAsync();
+            return Ok(await context.superHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var dbHero = await context.superHeroes.FindAsync(request.Id);
+            if(dbHero == null)
+            {
+                return NotFound();
+            }
+            dbHero.Name = request.Name;
+            dbHero.firstName = request.firstName;
+            dbHero.lastName = request.lastName;
+            dbHero.locality = request.locality;
+            await context.SaveChangesAsync();
+            return Ok(await context.superHeroes.ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            var thisHero = await context.superHeroes.FindAsync(heroId);
+            if(thisHero == null)
+            {
+                return NotFound("Hero not Founded.");
+            }
+            context.superHeroes.Remove(thisHero);
+            await context.SaveChangesAsync();
+            return Ok(await context.superHeroes.ToListAsync());
+        }
+    }
+}
---
+++ WebApp/Migrations/20220711172640_CreateInitial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "superHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    firstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    locality = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_superHeroes", x => x.Id);
+                });
+        }
+
+        
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "superHeroes");
+        }
+    }
+}
